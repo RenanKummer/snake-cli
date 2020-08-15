6d287f3a3236abb192df00422e9865882f049477
Renan Kummer <dev.renankummer@outlook.com>
2020-08-15 19:53:28 -0300

    Refactor test

--- test/engine/CommandLineInterfaceTest.c
+++ test/engine/CommandLineInterfaceTest.c
@@ -1,186 +1,49 @@
 #include "engine/CommandLineInterface.h"
-#include <windows.h>
+/**
+ * This is a manual test.
+ * 
+ * @author Renan Kummer [ GitHub: @RenanKummer ]
+ */
+#include <stdlib.h>
 #include <stdio.h>
-
-typedef enum Option
-{
-    MOVE_CURSOR   = 1,
-    WAIT          = 2,
-    RESIZE_WINDOW = 3,
-    CLEAR_WINDOW  = 4,
-    KEYBOARD_HIT  = 5
-} Option;
-
-void printOptions();
-Option readOption();
-
-Boolean isMoveCursorOption(Option option);
-Boolean isWaitOption(Option option);
-Boolean isResizeWindowOption(Option option);
-Boolean isClearWindowOption(Option option);
-Boolean isKeyboardHitOption(Option option);
-void runMoveCursor();
-void runWait();
-void runResizeWindow();
-void runClearWindow();
-void runKeyboardHit();
 
 int main()
 {
-    while (true) {
-        printOptions();
-        Option option = readOption();
+    printf("Disclaimer: This test must be run in a resizable command line window!\n");
 
-        if (isMoveCursorOption(option)) {
-            runMoveCursor();
-        } else if (isWaitOption(option)) {
-            runWait();
-        } else if (isResizeWindowOption(option)) {
-            runResizeWindow();
-        } else if (isClearWindowOption(option)) {
-            runClearWindow();
-        } else if (isKeyboardHitOption(option)) {
-            runKeyboardHit();
-        }
-    }    
-}
+    WindowSize newSize = {30, 100};
+    printf("Resizing window to {%d, %d}\n", newSize.height, newSize.width);
+    wait(5000);
+    resizeWindow(newSize);
 
-void printOptions()
-{
-    printf(
-        "Test CommandLineInterface\n"
-        "  %2d: moveCursor()\n"
-        "  %2d: wait()\n"
-        "  %2d: resizeWindow()\n"
-        "  %2d: clearWindow()\n"
-        "  %2d: keyboardHit()\n"
-        "\n",
-        MOVE_CURSOR,
-        WAIT,
-        RESIZE_WINDOW, 
-        CLEAR_WINDOW,
-        KEYBOARD_HIT
+    WindowCoordinate cursorCoordinate = {9, 3};
+    printf("Moving cursor to {%d, %d}\n", 
+        cursorCoordinate.height, 
+        cursorCoordinate.width
     );
-}
+    moveCursor(newSize, cursorCoordinate);
 
-Option readOption()
-{
-    Option option;
-    printf("Your option: ");
-    scanf("%d", &option);
-    printf("\n");
+    wait(5000);
 
-    return option;
-}
+    cursorCoordinate.height = 30;
+    printf("Attempting to move cursor to out of range position {%d, %d}\n", 
+        cursorCoordinate.height,
+        cursorCoordinate.width
+    );
+    moveCursor(newSize, cursorCoordinate);
 
-Boolean isMoveCursorOption(Option option)
-{
-    return option == MOVE_CURSOR;
-}
+    wait(5000);
 
-Boolean isWaitOption(Option option)
-{
-    return option == WAIT;
-}
-
-Boolean isResizeWindowOption(Option option)
-{
-    return option == RESIZE_WINDOW;
-}
-
-Boolean isClearWindowOption(Option option)
-{
-    return option == CLEAR_WINDOW;
-}
-
-Boolean isKeyboardHitOption(Option option)
-{
-    return option == KEYBOARD_HIT;
-}
-
-void runMoveCursor()
-{
-    int height;
-    printf("  Window height: ");
-    scanf("%d", &height);
-
-    int width;
-    printf("  Window width: ");
-    scanf("%d", &width);
-
-    WindowSize windowSize = {height, width};
-    resizeWindow(windowSize);
-
-    int moveToHeight;
-    printf("  Move to height: ");
-    scanf("%d", &moveToHeight);
-
-    int moveToWidth;
-    printf("  Move to width: ");
-    scanf("%d", &moveToWidth);
-
-    clearWindow();
-    WindowCoordinate coordinate = {moveToHeight, moveToWidth};
-    moveCursor(windowSize, coordinate);
-
-    printf("X");
-    WindowCoordinate firstCoordinate = {0, 0};
-    moveCursor(windowSize, firstCoordinate);    
-    wait(3000);
-    
-    clearWindow();
-}
-
-void runWait()
-{
-    printf("Waiting 3 seconds...\n");
-    wait(3000);
-    printf("Waiting 2 seconds...\n");
-    wait(2000);
-    printf("Waiting 1 seconds...\n");
-    wait(1000);
-    printf("And we're done!\n\n");
-}
-
-void runResizeWindow()
-{
-    int height;
-    printf("  Height: ");
-    scanf("%d", &height);
-
-    int width;
-    printf("  Width: ");
-    scanf("%d", &width);
-
-    WindowSize windowSize = {height, width};
-    resizeWindow(windowSize);
-}
-
-void runClearWindow()
-{
-    printf(
-        "  Some text here!\n"
-        "  More over here...\n"
-        "\n"
-        "  And last, but not least, here.\n"
+    printf("I must be at the default position {%d, %d}\n", 
+        getDefaultWindowCoordinate().height,
+        getDefaultWindowCoordinate().width
     );
 
-    wait(1000);
-
+    printf("Waiting 5 seconds before clearing all output...");
+    wait(5000);
     clearWindow();
-}
 
-void runKeyboardHit()
-{
-    printf("Waiting for keyboard hit...\n");
-
-    while (!hasKeyboardHit()) {
-        // Do nothing.
-    }
-
-    int key = readKeyboardHitNoEcho();
-    printf("You entered key %d.\n", key);
-
-    wait(3000);
-    clearWindow();
+    printf("Waiting for any key to be entered...\n");
+    while (!hasKeyboardHit()) {}
+    printf("You entered key %d\n", readKeyboardHitNoEcho());
 }
